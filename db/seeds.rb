8e4e2df0dc63fffc5ad9d911d84d021195e3d3c8
rscheiwe <rscheiwe@gmail.com>
2018-08-31 17:48:56 -0400

    updated seed algo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# require 'smarter_csv'
-#
-# options= {}
-#
-# SmarterCSV.process('./test_for_migrating.csv', options) do |chunk|
-#   chunk.each do |data_hash|
-#     Course.create!( data_hash )
-#   end
-# end
+
 require 'csv'
 csv_text = File.read(Rails.root.join('db', './test_for_migrating.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
