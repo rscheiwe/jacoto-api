9cf1b53c7cd164a1c68320797ea3a46004c63489
rscheiwe <rscheiwe@gmail.com>
2018-09-10 17:14:45 -0400

    updated join table

--- app/controllers/api/v1/user_courses_controller.rb
+++ app/controllers/api/v1/user_courses_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::UserCoursesController < ApplicationController
   skip_before_action :authorized, only: [:index]
 
-  def index
-    @user_courses = UserCourse.all.includes(:user).as_json(include: { user: { only: [:username] } })
+  # def index
+  #   @user_courses = UserCourse.all.includes(:user).as_json(include: { user: { only: [:username] } })
+  # end
+
+  def create
+
   end
 end
