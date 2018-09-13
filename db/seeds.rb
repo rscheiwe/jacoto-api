507b82bb60ce5b8e1982b47cb585e3b9a3ae79d4
rscheiwe <rscheiwe@gmail.com>
2018-09-13 12:40:25 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 require 'csv'
-csv_text = File.read(Rails.root.join('db', './Book8.csv'))
+csv_text = File.read(Rails.root.join('db', './Book9.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
   # byebug
